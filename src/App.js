4cfb9105c0fe7bee8d223e4b8e25be229482a761
Noibisjunior <noibisjunior22@gmail.com>
2023-02-02 11:05:40 +0100

    new branch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import Auth from './components/Auth';
 import './App.css';
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Otp from './pages/Otp';
 
 function App() {
-  return (
-    <div className="App">
-     <Auth/>
-    
-    </div>
-  );
+  
+    return (
+      <>
+     <Router>
+    <Routes>
+      <Route path="/" element={<Auth />}/>
+      <Route path="/Otp" element={<Otp />}/>
+      </Routes>
+    </Router> 
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
+    </>
+    );
+  
+  
 }
 
 export default App;
--- src/components/Otp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Otp = () => {
-  return (
-    <div>Otpfff</div>
-  )
-}
-
-export default Otp
